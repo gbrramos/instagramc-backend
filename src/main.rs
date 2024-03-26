aac36a703529f79718d25c5460f4f0073eaadb00
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2024-03-26 09:42:13 +0000

    first sync

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello, 123!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
